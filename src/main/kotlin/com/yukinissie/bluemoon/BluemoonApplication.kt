5a697fd7d4aa971130be9e1a217c8e065112112b
yukinissie <bigtree2490@outlook.jp>
2022-05-26 17:41:53 +0900

    init

---
+++ src/main/kotlin/com/yukinissie/bluemoon/BluemoonApplication.kt
@@ -0,0 +1,11 @@
+package com.yukinissie.bluemoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BluemoonApplication
+
+fun main(args: Array<String>) {
+    runApplication<BluemoonApplication>(*args)
+}
---
+++ src/test/kotlin/com/yukinissie/bluemoon/BluemoonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yukinissie.bluemoon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BluemoonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
